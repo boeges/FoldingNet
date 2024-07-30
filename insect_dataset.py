9ecf88363ead04fe3692b7ee1d93292c4a76ad33
boeges <boeges>
2024-07-30 09:28:48 +0200

    insect dataset

--- insect_dataset.py
+++ insect_dataset.py
@@ -12,6 +12,7 @@ import torch
 import json
 import h5py
 from glob import glob
+from pathlib import Path
 import numpy as np
 import torch.utils.data as data
 
@@ -40,18 +41,18 @@ def rotate_pointcloud(pointcloud):
 class InsectDataset(data.Dataset):
     def __init__(self, root, dataset_name='modelnet40', 
             num_points=2048, split='train', load_name=False,
-            random_rotate=False, random_jitter=False, random_translate=False):
+            random_rotate=False, random_jitter=False, random_translate=False,
+            classes=None, use_classes=None):
+        """
+        Args:
+            root (str): Path of the parent directory of the dataset
+            classes (_type_, optional): Class list ordered by id, beginning at 0. Defaults to CLASSES_6B.
+            use_classes (_type_, optional): Load samples of only these classes. Defaults to None = all classes.
+        """
 
-        assert dataset_name.lower() in ['shapenetcorev2', 
-            'shapenetpart', 'modelnet10', 'modelnet40']
-        assert num_points <= 2048        
+        assert num_points <= 2048 # why?
 
-        if dataset_name in ['shapenetpart', 'shapenetcorev2']:
-            assert split.lower() in ['train', 'test', 'val', 'trainval', 'all']
-        else:
-            assert split.lower() in ['train', 'test', 'all']
-
-        self.root = os.path.join(root, dataset_name + '*hdf5_2048')
+        self.root = os.path.join(root, dataset_name)
         self.dataset_name = dataset_name
         self.num_points = num_points
         self.split = split
@@ -59,59 +60,35 @@ class InsectDataset(data.Dataset):
         self.random_rotate = random_rotate
         self.random_jitter = random_jitter
         self.random_translate = random_translate
+        self.classes = classes
+        self.use_classes = use_classes
         
-        self.path_h5py_all = []
-        self.path_json_all = []
-        if self.split in ['train','trainval','all']:   
-            self.get_path('train')
-        if self.dataset_name in ['shapenetpart', 'shapenetcorev2']:
-            if self.split in ['val','trainval','all']: 
-                self.get_path('val')
-        if self.split in ['test', 'all']:   
-            self.get_path('test')
+        # dataset directory
+        self.root = root
+        # <class_name>:<class_id>
+        self.class_id_map = dict(zip(classes, range(len(classes))))
+        # <class_id>:<class_name>
+        self.id_class_map = {v:k for k,v in self.class_id_map.items()}
 
-        self.path_h5py_all.sort()
-        data, label = self.load_h5py(self.path_h5py_all)
-        if self.load_name:
-            self.path_json_all.sort()
-            self.name = self.load_json(self.path_json_all)    # load label name
-        
-        self.data = np.concatenate(data, axis=0)
-        self.label = np.concatenate(label, axis=0) 
+        self.samples = []
+        skipped_count = 0
+        for f in Path(root).glob("*/*.csv"):
+            clas = f.parent.name
+            if use_classes is not None and clas not in use_classes:
+                # skip this sample if class is not used
+                skipped_count += 1
+                continue
+            cla_id = self.class_id_map[clas]
+            point_set = np.loadtxt(f, delimiter=',', skiprows=1, max_rows=self.num_points, usecols=(0,1,2)).astype(np.float32)
+            rel_path = str(Path(clas) / f.name)
+            self.samples.append( (point_set, cla_id, rel_path) )
+        print(f"Loaded dataset from {self.root}; {skipped_count} samples skipped (not in use_classes)!")
 
-    def get_path(self, type):
-        path_h5py = os.path.join(self.root, '*%s*.h5'%type)
-        self.path_h5py_all += glob(path_h5py)
-        if self.load_name:
-            path_json = os.path.join(self.root, '%s*_id2name.json'%type)
-            self.path_json_all += glob(path_json)
-        return 
-
-    def load_h5py(self, path):
-        all_data = []
-        all_label = []
-        for h5_name in path:
-            f = h5py.File(h5_name, 'r+')
-            data = f['data'][:].astype('float32')
-            label = f['label'][:].astype('int64')
-            f.close()
-            all_data.append(data)
-            all_label.append(label)
-        return all_data, all_label
-
-    def load_json(self, path):
-        all_data = []
-        for json_name in path:
-            j =  open(json_name, 'r+')
-            data = json.load(j)
-            all_data += data
-        return all_data
-
-    def __getitem__(self, item):
-        point_set = self.data[item][:self.num_points]
-        label = self.label[item]
-        if self.load_name:
-            name = self.name[item]  # get label name
+    def __getitem__(self, index):
+        sample = self.samples[index]
+        point_set = sample[0]
+        class_id = sample[1]
+        # rel_path = sample[2]
 
         if self.random_rotate:
             point_set = rotate_pointcloud(point_set)
@@ -126,9 +103,10 @@ class InsectDataset(data.Dataset):
         label = label.squeeze(0)
         
         if self.load_name:
-            return point_set, label, name
+            class_name = self.id_class_map[class_id]
+            return point_set, label, class_name
         else:
             return point_set, label
 
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.samples)
